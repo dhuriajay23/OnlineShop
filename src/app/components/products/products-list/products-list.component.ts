487c4ad8eb835a0bb2beae4233ae449a14161e77
Ajay Dhuri <dhuriajay23@gmail.com>
2023-06-09 23:11:24 +0530

    Created static products list.

--- src/app/components/products/products-list/products-list.component.ts
+++ src/app/components/products/products-list/products-list.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../../models/product.model';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css']
 })
-export class ProductsListComponent {
+export class ProductsListComponent implements OnInit {
+
+  products: Product[] = [
+    {
+      id : '1',
+      name : 'CD',
+      category : 'Storage',
+      price : 100,
+      quantity : 20
+    },
+    {
+      id: '2',
+      name: 'HD',
+      category: 'Storage',
+      price: 200,
+      quantity: 15
+    },
+    {
+      id: '3',
+      name: 'SSD',
+      category: '',
+      price: 500,
+      quantity: 5
+    }
+  ];
+
+  constructor() {
+
+  }    
+
+  ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
 
 }
