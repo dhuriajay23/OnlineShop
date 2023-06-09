941c8936f8524522320fe4eba2659041a8c6e1f9
Ajay Dhuri <dhuriajay23@gmail.com>
2023-06-09 19:21:17 +0530

    Added routing for products component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsListComponent } from './components/products/products-list/products-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductsListComponent },
+  { path: 'products', component: ProductsListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
